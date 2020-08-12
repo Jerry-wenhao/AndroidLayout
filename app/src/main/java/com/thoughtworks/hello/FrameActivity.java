cddbb4e66f06d73cbac1f811d0ed098c8e7223fe
Jerry777777 <491818175@qq.com>
2020-08-12 12:50:43 +0800

    [xiawenhao] feat: click button_back can back to the mainactivity

--- app/src/main/java/com/thoughtworks/hello/FrameActivity.java
+++ app/src/main/java/com/thoughtworks/hello/FrameActivity.java
@@ -1,13 +1,24 @@
 package com.thoughtworks.hello;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FrameActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_layout);
-    }
+        findViewById(R.id.button_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FrameActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
 }
+    }
 
