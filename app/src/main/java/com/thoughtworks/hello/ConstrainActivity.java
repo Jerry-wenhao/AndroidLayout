57dd4520e5ac10425cdbde452606c6d444538b5e
Jerry777777 <491818175@qq.com>
2020-08-12 13:26:32 +0800

    [xiawenhao] feat: creat constrain_layout

---
+++ app/src/main/java/com/thoughtworks/hello/ConstrainActivity.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConstrainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedsavedInstanceStat) {
+        super.onCreate(savedsavedInstanceStat);
+        setContentView(R.layout.activity_constrain_layout);
+    }
+}
+
